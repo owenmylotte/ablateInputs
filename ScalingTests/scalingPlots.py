273c2c8ab5c618d27fa1e1fdbfa793fc22ce396c
owenmylotte <owenmylotte@gmail.com>
2022-11-03 11:17:00 -0400

    Plots update

--- ScalingTests/scalingPlots.py
+++ ScalingTests/scalingPlots.py
@@ -5,16 +5,17 @@ import itertools
 from os.path import exists
 
 # Set up options that must be defined by the user
-colorarray = ["darkgrey", "grey", "burlywood", "brown", "firebrick", "darkorange", "gold", "yellowgreen",
-              "green", "mediumaquamarine", "mediumturquoise", "powderblue", "mediumpurple", "mediumorchid",
-              "deeppink"]
+colorarray = ["black", "grey", "firebrick", "lightcoral", "darkorange", "goldenrod", "yellow", "yellowgreen",
+              "green", "lightgreen", "teal", "powderblue", "darkorchid", "violet",
+              "palevioletred"]
 markerarray = [".", "1", "P", "*"]
 
 # Define the arrays that contain the options which were used
 processes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
 faces = ["[105,15]", "[149,21]", "[210,30]", "[297,42]", "[420,60]", "[594,85]", "[840,120]"]
-rays = [10, 25, 50, 200]
-dims = "2D"
+rays = np.array([10, 25, 50, 250])
+dtheta = 180 / rays
+dims = " 2D Non-Sharing"
 
 # Template path: "outputs/Scaling2D_30_16_[105, 15].xml"
 basePath = "outputs/Scaling"
@@ -43,7 +44,7 @@ for r in range(len(rays)):
     for p in range(len(processes)):
         for f in range(len(faces)):
             # Create strings which represent the file names of the outputs
-            path = basePath + dims + "_" + str(rays[r]) + "_" + str(processes[p]) + "_" + str(
+            path = basePath + "2D" + "_" + str(rays[r]) + "_" + str(processes[p]) + "_" + str(
                 faces[f]) + ".xml"  # File path
             # path = "outputs/scalingTests2D_10_1_[105, 15].xml"  # Hack for testing
             if exists(path):  # Make sure not to try accessing a path that doesn't exist
@@ -68,10 +69,14 @@ processes = np.asarray(processes)
 faces = np.asarray(faces)
 rays = np.asarray(rays)
 
+f = lambda m, c: plt.plot([], [], marker=m, color=c, ls="none")[0]
+handles = [f(markerarray[i], "k") for i in range(len(markerarray))]
+handles += [f("s", colorarray[i]) for i in range(len(colorarray))]
+
 d = 0
 # Initialization static scaling analysis
-plt.figure(figsize=(6, 4), num=1)
-plt.title("Initialization Static Scaling", pad=1)
+plt.figure(figsize=(10, 6), num=1)
+plt.title("Initialization Static Scaling" + dims, pad=1)
 for n in range(len(rays)):
     for i in range(len(processes)):
         mask = np.isfinite(initTime[n, i, :])
@@ -82,13 +87,15 @@ plt.yticks(fontsize=10)
 plt.xticks(fontsize=10)
 plt.xlabel(r'DOF $[cells]$', fontsize=10)
 plt.ylabel(r'Performance $[\frac{DOF}{s}]$', fontsize=10)
-# plt.legend(processes, loc="upper left")
+labels = dtheta
+labels = np.append(labels, processes)
+plt.legend(handles, labels, loc="upper left")
 plt.savefig('initScalingStatic' + dims, dpi=1500, bbox_inches='tight')
 plt.show()
 
 # Initialization Strong scaling analysis
-plt.figure(figsize=(6, 4), num=1)
-plt.title("Initialization Strong Scaling", pad=1)
+plt.figure(figsize=(10, 6), num=2)
+plt.title("Initialization Strong Scaling" + dims, pad=1)
 for n in range(len(rays)):
     for i in range(len(faces)):
         mask = np.isfinite(initTime[n, :, i])
@@ -105,48 +112,68 @@ plt.yticks(fontsize=10)
 plt.xticks(fontsize=10)
 plt.xlabel(r'Processes', fontsize=10)
 plt.ylabel(r'Speedup', fontsize=10)
-# plt.legend(faces, loc="upper left")
+labels = dtheta
+labels = np.append(labels, faces)
+plt.legend(handles, labels, loc="upper left")
 plt.savefig('initScalingStrong' + dims, dpi=1500, bbox_inches='tight')
 plt.show()
 
-# # Initialization Weak scaling analysis
-# weakInit = np.zeros([len(processes) + len(faces), len(processes) * len(faces)])
-# for i in range(-(len(processes)), len(faces)):
-#     weakInit[(i+len(processes)), :] = np.diag(initTime[0, :, :], k=i)
-#
+# Initialization Weak scaling analysis
+# weakInit = np.zeros([len(rays), len(processes) + len(faces), len(faces)])
+# I = len(processes)
+# for n in range(len(rays)):
+#     for i in range(len(processes)):
+#         for j in range(len(faces)):
+#             x = ((I-1) - i) + j
+#             weakInit[n, x, j] = initTime[n, i, j]
+#             if weakInit[n, x, j] == 0:
+#                 weakInit[n, x, j] = float("nan")
+
 # plt.figure(figsize=(6, 4), num=1)
 # plt.title("Initialization Weak Scaling", pad=1)
-# for i in range(len(faces)):
-#     plt.loglog(processes, initTime[0, :, i], linewidth=1, marker='.')
+# I = len(processes)
+# for n in range(len(rays)):
+#     for i in range(len(faces) + len(processes)):
+#         weakInit = np.diagonal(initTime, offset=(i-I), axis1=0, axis2=1)
+#         mask = np.isfinite(weakInit)
+#         x = cellsize[d, :]
+#         y = cellsize[d, :] / weakInit[:]
+#         # Bring the lowest available index to the line to normalize the scaling plot * (ideal / lowest available index)
+#         first = np.argmax(mask)
+#
+#         plt.loglog(x[mask], (cellsize[first] * y[first]) / y[mask], linewidth=1, marker='.')
 # plt.yticks(fontsize=10)
 # plt.xticks(fontsize=10)
-# plt.xlabel(r'Processes $[s]$', fontsize=10)
-# plt.ylabel(r'Performance $[\frac{DOF}{s}]$', fontsize=10)
-# plt.legend(faces, loc="upper left")
+# plt.xlabel(r'DOF $[cells]$', fontsize=10)
+# plt.ylabel(r'Efficiency', fontsize=10)
+# # plt.legend(faces, loc="upper left")
 # plt.savefig('scalingWeak' + dims, dpi=1500, bbox_inches='tight')
 # plt.show()
 
+
 # Solve static scaling analysis
-plt.figure(figsize=(6, 4), num=1)
-plt.title("Solve Static Scaling", pad=1)
+plt.figure(figsize=(10, 6), num=3)
+plt.title("Solve Static Scaling" + dims, pad=1)
 for n in range(len(rays)):
     for i in range(len(processes)):
         mask = np.isfinite(solveTime[n, i, :])
         x = cellsize[d, :]
         y = cellsize[d, :] / solveTime[n, i, :]
         plt.loglog(x[mask], y[mask], linewidth=1, marker=markerarray[n],
-                   c=colorarray[i])  # TODO: Rays by marker style, p-size by line color
+                   c=colorarray[i])
 plt.yticks(fontsize=10)
 plt.xticks(fontsize=10)
 plt.xlabel(r'DOF $[cells]$', fontsize=10)
 plt.ylabel(r'Performance $[\frac{DOF}{s}]$', fontsize=10)
-# plt.legend(processes, loc="upper left")  # TODO: Legend should show line color and marker style meaning
+labels = dtheta
+labels = np.append(labels, processes)
+plt.legend(handles, labels, loc="upper left")
 plt.savefig('solveScalingStatic' + dims, dpi=1500, bbox_inches='tight')
 plt.show()
 
 # Initialization Strong scaling analysis
-plt.figure(figsize=(6, 4), num=1)
-plt.title("Solve Strong Scaling", pad=1)
+plt.figure(figsize=(10, 6), num=4)
+plt.title("Solve Strong Scaling" + dims, pad=1)
 for n in range(len(rays)):
     for i in range(len(faces)):
         mask = np.isfinite(solveTime[n, :, i])
@@ -163,6 +190,8 @@ plt.yticks(fontsize=10)
 plt.xticks(fontsize=10)
 plt.xlabel(r'Processes', fontsize=10)
 plt.ylabel(r'Speedup', fontsize=10)
-# plt.legend(faces, loc="upper left")
+labels = dtheta
+labels = np.append(labels, faces)
+plt.legend(handles, labels, loc="upper left")
 plt.savefig('solveScalingStrong' + dims, dpi=1500, bbox_inches='tight')
 plt.show()
