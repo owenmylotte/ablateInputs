1005817fefd1a4bf536e5ce08d80a865adcc6d4e
owenmylotte <owenmylotte@gmail.com>
2023-04-28 20:14:49 -0400

    Strong scaling working correctly.

--- ScalingTests/scalingPlotsBlack.py
+++ ScalingTests/scalingPlotsBlack.py
@@ -16,28 +16,49 @@ import pathlib
 
 class PerformanceAnalysis:
 
-    def __init__(self, base_path=None, name=None, processes=None, faces=None, cellsize=None, rays=None, events=None):
-        self.basePath = base_path
+    def __init__(self, base_path=None, name=None, processes=None, faces=None, cell_size=None, rays=None, events=None):
+        self.times = None
+        self.markerarray = None
+        self.colorarray = None
+        self.base_path = base_path
         self.name = name
         self.processes = processes
         self.faces = faces
-        self.cellsize = cellsize
-        self.events = events
+        self.cell_size = cell_size
+        self.events = np.asarray(events, dtype=bytes)
         self.rays = rays
+        self.set_plot_parameters()
 
     @staticmethod
     def r_squared_func(y, y_fit):
         return 1 - np.sum((y - y_fit) ** 2) / np.sum((y - np.mean(y)) ** 2)
 
+    # Do curve fitting of the data for performance modelling purposes.
+    @staticmethod
+    def gustafson_func(n, t0, s, c, d, f):
+        return t0 * np.log(n) + s * np.log(n) * np.log(n) + d * np.log(n) * np.log(n) * np.log(n) + c * n ** f
+
+    def set_plot_parameters(self):
+        # Set up plotting options that must be defined by the user
+        self.colorarray = ["o", "s", "o", "s", ".", ".", ".", ".",
+                           ".", ".", ".", ".", ".", ".",
+                           "."]
+        self.markerarray = [".", "1", "P", "*"]
+        plt.rcParams["font.family"] = "Noto Serif CJK JP"
+
+        f = lambda m, c: plt.plot([], [], marker=m, color=c, ls="none")[0]
+        handles = [f(self.markerarray[i], "k") for i in range(len(self.markerarray))]
+        handles += [f("s", "black") for i in range(len(self.colorarray))]
+
     def load_csv_files(self):
         # Create arrays which the parsed information will be stored inside: Whatever information is desired
-        times = np.zeros(len(self.events), len(self.rays), len(self.processes), len(self.faces))
+        self.times = np.zeros([len(self.events), len(self.rays), len(self.processes), len(self.faces)])
         # Iterate through the arrays to get information out of the files
         for r in range(len(self.rays)):
             for p in range(len(self.processes)):
                 for f in range(len(self.faces)):
                     # Create strings which represent the file names of the outputs
-                    path = self.base_path + self.name + "_" + str(self.rays[r]) + "_" + str(
+                    path = self.base_path + "/" + self.name + "_" + str(self.rays[r]) + "_" + str(
                         self.processes[p]) + "_" + str(
                         self.faces[f]) + ".csv"  # File path
                     dtypes = {'names': ('stage', 'name', 'time'),
@@ -50,27 +71,17 @@ class PerformanceAnalysis:
 
                             for i in range(lines):  # Iterate through all the lines in the csv
                                 if (data[i][1] == self.events[e]) and (
-                                        data[i][2] > times[e, r, p, f]):  # Check current line
-                                    times[e, r, p, f] = data[i][
+                                        data[i][2] > self.times[e, r, p, f]):  # Check current line
+                                    self.times[e, r, p, f] = data[i][
                                         2]  # If it is, then write the value in column 4 of that line
 
                             # If the time is never written then the filter code then don't try to plot it
-                            if times[e, r, p, f] == 0:
-                                times[e, r, p, f] = float("nan")
+                            if self.times[e, r, p, f] == 0:
+                                self.times[e, r, p, f] = float("nan")
 
         self.processes = np.asarray(self.processes)
         self.faces = np.asarray(self.faces)
         self.rays = np.asarray(self.rays)
-
-    # Do curve fitting of the data for performance modelling purposes.
-    @staticmethod
-    def gustafson_func(self, N, t0, s, c, d, f):
-        # intensity = s / N
-        # performance = np.min(N, d)
-        # return t0 * (N ** f) + c * performance * np.log10(N)
-        # return t0 * (1 / ((N ** c) * performance) +  * np.log(N))
-        # return t0 * (1 / ((N ** c) * s) + (1 - 1 / (N ** c)) * np.log(N))
-        return t0 * np.log(N) + s * np.log(N) * np.log(N) + d * np.log(N) * np.log(N) * np.log(N) + c * N ** f
 
     # Set bounds for the parameters (all non-negative)
     # param_bounds = ([0, -np.inf, 0, -np.inf, 0], [np.inf, np.inf, np.inf, np.inf, np.inf])
@@ -84,17 +95,17 @@ class PerformanceAnalysis:
             for n in range(len(rays)):
                 for i in range(len(self.processes)):
                     mask = np.isfinite(self.times[e, n, i, :])
-                    x = self.cellsize
-                    y = self.cellsize / self.times[e, n, i, :]
-                    plt.loglog(x[mask], y[mask], linewidth=1, marker=markerarray[n], c=colorarray[i])
+                    x = self.cell_size
+                    y = self.cell_size / self.times[e, n, i, :]
+                    plt.loglog(x[mask], y[mask], linewidth=1, marker=self.markerarray[n], c=self.colorarray[i])
             plt.yticks(fontsize=10)
             plt.xticks(fontsize=10)
             plt.xlabel(r'DOF $[cells]$', fontsize=10)
             plt.ylabel(r'Performance $[\frac{DOF}{s}]$', fontsize=10)
             labels = dtheta
             labels = np.append(labels, self.processes)
-            plt.legend(handles, labels, loc="upper left")
-            plt.savefig('StaticScaling_' + self.events[e] + ".png", dpi=1500, bbox_inches='tight')
+            plt.legend(self.handles, labels, loc="upper left")
+            plt.savefig(self.name + str(self.events[e]) + '_static_scaling.png', dpi=1500, bbox_inches='tight')
             plt.show()
 
     def plot_weak_scaling(self):
@@ -117,19 +128,19 @@ class PerformanceAnalysis:
                 for i in range(len(self.faces) + len(self.processes)):
                     weak_init = np.diagonal(self.times, offset=(i - I), axis1=0, axis2=1)
                     mask = np.isfinite(weak_init)
-                    x = self.cellsize
-                    y = self.cellsize / weak_init
+                    x = self.cell_size
+                    y = self.cell_size / weak_init
                     # Bring the lowest available index to the line to normalize the scaling plot *
                     # (ideal / lowest available index)
                     first = np.argmax(mask)
 
-                    plt.loglog(x[mask], (self.cellsize[first] * y[first]) / y[mask], linewidth=1, marker='.')
+                    plt.loglog(x[mask], (self.cell_size[first] * y[first]) / y[mask], linewidth=1, marker='.')
             plt.yticks(fontsize=10)
             plt.xticks(fontsize=10)
             plt.xlabel(r'DOF $[cells]$', fontsize=10)
             plt.ylabel(r'Efficiency', fontsize=10)
             # plt.legend(faces, loc="upper left")
-            plt.savefig('WeakScaling_' + self.events[e] + ".png", dpi=1500, bbox_inches='tight')
+            plt.savefig(self.name + str(self.events[e]) + '_weak_scaling.png', dpi=1500, bbox_inches='tight')
             plt.show()
 
     def plot_strong_scaling(self, function_fit):
@@ -147,7 +158,7 @@ class PerformanceAnalysis:
                     # (ideal / lowest available index)
                     first = np.argmax(mask)
 
-                    if np.any(mask):
+                    if np.sum(mask) > 5:
                         # Perform non-linear curve fitting
                         popt, pcov = curve_fit(function_fit, x[mask], y[mask])  # , bounds=param_bounds)
 
@@ -162,7 +173,7 @@ class PerformanceAnalysis:
                                    label=f'Fitted curve: t0={t0:.2f}, s={s:.2f}, c={c:.2f}, r^2={r_squared:.2f}')
                         print(f't0={t0:.2f}, s={s:.2f}, c={c:.2f}, d={d:.2f}, f={f:.2f}, r^2={r_squared:.2f}')
                     adjusted = (self.processes[first] * y[first]) / y[mask]
-                    plt.loglog(x[mask], adjusted, linewidth=1, marker=colorarray[i],
+                    plt.loglog(x[mask], adjusted, linewidth=1, marker=self.colorarray[i],
                                c="black", markersize=4)
             plt.plot(self.processes, self.processes, linewidth=1, c="black", linestyle="--")
             plt.yticks(fontsize=10)
@@ -173,14 +184,14 @@ class PerformanceAnalysis:
             # labels = np.append(labels, faces)
             # plt.legend(["2D"], loc="upper left")  # , "3D"
             # plt.legend()
-            plt.savefig('StrongScaling_' + self.events[e] + ".png", dpi=1500, bbox_inches='tight')
+            plt.savefig(self.name + str(self.events[e]) + '_strong_scaling.png', dpi=1500, bbox_inches='tight')
             plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Process scaling data from PETSc events.')
-    parser.add_argument('--path', dest='base_path', type=pathlib.Path, required=True,
+    parser.add_argument('--path', dest='base_path', type=str, required=True,
                         help='Path to the base directory containing scaling csv files.')
     parser.add_argument('--name', dest='name', type=str, required=True,
                         help='Title of the files being processed.')
@@ -188,7 +199,7 @@ if __name__ == "__main__":
                         help='Process counts being considered.')
     parser.add_argument('--problems', dest='problems', type=str, required=True, nargs="+",
                         help='Problems being considered.')
-    parser.add_argument('--dofs', dest='cellsize', type=float, required=True, nargs="+",
+    parser.add_argument('--dof', dest='cell_size', type=float, required=True, nargs="+",
                         help='Mesh or dof size associated with each problem.')
     parser.add_argument('--events', dest='events', type=str, required=True, nargs="+",
                         help='Event names to measure.')
@@ -197,18 +208,11 @@ if __name__ == "__main__":
     rays = np.array([5, 10, 25, 50])
 
     scaling_data = PerformanceAnalysis(args.base_path, args.name, args.processes,
-                                       args.problems, args.cellsize, rays, args.events)
-
-    # Set up plotting options that must be defined by the user
-    colorarray = ["o", "s", "o", "s", ".", ".", ".", ".",
-                  ".", ".", ".", ".", ".", ".",
-                  "."]
-    markerarray = [".", "1", "P", "*"]
-    plt.rcParams["font.family"] = "Noto Serif CJK JP"
-
-    f = lambda m, c: plt.plot([], [], marker=m, color=c, ls="none")[0]
-    handles = [f(markerarray[i], "k") for i in range(len(markerarray))]
-    handles += [f("s", "black") for i in range(len(colorarray))]
+                                       args.problems, args.cell_size, rays, args.events)
+    scaling_data.load_csv_files()
+    scaling_data.plot_strong_scaling(scaling_data.gustafson_func)
+    # scaling_data.plot_weak_scaling()
+    # scaling_data.plot_static_scaling()
 
     # Define the arrays that contain the options which were used
     # processes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]  # , 8192, 16384, 32768]
